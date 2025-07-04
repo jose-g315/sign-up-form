7daf4774dd31651f0a302b8d5eaa8a207fd8f5e6
jose-g315 <120354063+jose-g315@users.noreply.github.com>
2025-07-03 20:15:55 -0500

    Change layout and update content

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 // form validation
 
 // adding dashes to phone number input
-const telephoneNumber = document.querySelector('#number');
-telephoneNumber.addEventListener('keyup', () =>{
-  if (event.key != 'Backspace' && (telephoneNumber.value.length === 3 || telephoneNumber.value.length === 7)){
+const telephoneNumber = document.querySelector('#pNumber');
+telephoneNumber.addEventListener('keyup', (e) =>{
+  if (e.key != 'Backspace' && (telephoneNumber.value.length === 3 || telephoneNumber.value.length === 7)){
   telephoneNumber.value += '-';
   }
 });
